3c17a417729df84beecaf224d72e2a0fc9fc5bb5
Masakazu Kanagawa <kanagawa.m@gamil.com>
2019-09-15 17:56:57 +0900

    add export tilesource file.

---
+++ export defold tile animation.lua
@@ -0,0 +1,85 @@
+
+local sprite = app.activeSprite
+
+
+local dlg = Dialog()
+
+--- @param f FILE*
+--- @param tag any
+local function export_animation_tag(f, tag)
+  local fps = 10
+
+  local animation_type = "PLAYBACK_LOOP_FORWARD"
+  if tag.aniDir == AniDir.FORWARD then
+    animation_type = "PLAYBACK_LOOP_FORWARD"
+  elseif tag.aniDir == AniDir.REVERSE then
+    animation_type = "PLAYBACK_LOOP_BACKWARD"
+  elseif tag.aniDir == AniDir.PING_PONG then
+    animation_type = "PLAYBACK_LOOP_PINGPONG"
+  end
+
+  f:write("animations {\n")
+  f:write("  id: \""..tag.name.."\"\n")
+  f:write("  start_tile: "..tostring(tag.fromFrame.frameNumber).."\n")
+  f:write("  end_tile: "..tostring(tag.toFrame.frameNumber).."\n")
+  f:write("  playback: "..animation_type.."\n")
+  f:write("  fps: "..tostring(fps).."\n")
+  f:write("  flip_horizontal: 0\n")
+  f:write("  flip_vertical: 0\n")
+  f:write("}\n")
+end
+
+--- @param f FILE*
+local function export_header(f, sprite)
+  f:write("image: \"/\"\n")
+  f:write("tile_width: "..tostring(sprite.width).."\n")
+  f:write("tile_height: "..tostring(sprite.height).."\n")
+  f:write("tile_margin: 0\n")
+  f:write("tile_spacing: 0\n")
+  f:write("collision: \"\"\n")
+  f:write("material_tag: \"tile\"\n")
+  f:write("collision_groups: \"default\"\n")
+end
+
+--- @param f FILE*
+local function export_footer(f)
+  f:write("extrude_borders: 2\n")
+  f:write("inner_padding: 0\n")
+end
+
+local function export(data, sprite)
+
+  app.alert("The given value is '" .. data.game_project .. "'")
+  local f = io.open("write.txt", "w")
+
+  export_header(f, sprite)
+  for id, tag in ipairs(sprite.tags) do
+    export_animation_tag(f, tag)
+  end
+  export_footer(f)
+
+  f:close()
+end
+
+dlg:file{ id="export_file",
+          label="export file(tilesource)",
+          title="export file(tilesource)",
+          save=true,
+          filetypes={ "tilesource" },
+          onchange=nil }
+
+dlg:button{ text="Export",
+            onclick=function()
+                      local data = dlg.data
+                      export(data, sprite)
+                      dlg:close()
+                    end }
+dlg:button{ text="Close",
+            onclick=function()
+                      dlg:close()
+                    end }
+dlg:show{ wait=true }
+
+
+
+
